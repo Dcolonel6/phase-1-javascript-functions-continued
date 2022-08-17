6fa0e9483fefbaf60570f1e20e0b2c698788e834
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-17 15:11:31 +0300

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(funActivity = "roller-skate"){
+    return `This Saturday, I want to ${funActivity}!`;
+}
+function mondayWork(action = 'go to the office'){
+    return `This Monday, I will ${action}.`;
+}
+function wrapAdjective(visualFlare){
+
+    return function(param = 'special'){
+        return `You are ${visualFlare}${param}${visualFlare}!`;
+    };
+}
